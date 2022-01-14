26199934fc5156ccd18f7717049d717a17d34929
Martin Nestorov <mnestorov@protonmail.com>
2022-01-14 23:32:18 +0200

    Apply method to be tested in Runner
    This should allow for the simple test case to pass and validate the
    written code in the Runner.

---
+++ src/main/java/tdd/logic/Runner.java
@@ -0,0 +1,16 @@
+package tdd.logic;
+
+public class Runner {
+
+	public static void main(String[] args) {
+
+		Runner r = new Runner();
+
+		System.out.println(r.getBasicString());
+	}
+
+	public String getBasicString() {
+
+		return "basicTest";
+	}
+}
