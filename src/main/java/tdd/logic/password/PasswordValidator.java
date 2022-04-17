d34cd555642d6a9b3dcd12f14355b6217787aeb2
Martin Nestorov <mnestorov@protonmail.com>
2022-04-17 18:44:14 +0300

    Add password validation tests and implementation

--- src/main/java/tdd/logic/password/PasswordValidator.java
+++ src/main/java/tdd/logic/password/PasswordValidator.java
@@ -1,9 +1,50 @@
 package tdd.logic.password;
 
+import org.apache.commons.lang3.Validate;
+
 public class PasswordValidator {
 
 	public static boolean validatePassword(String password) throws NullPointerException {
 
-		return false;
+		Validate.notNull(password, "Password cannot be null!");
+
+		if (password.length() <= 11) {
+
+			return false;
+		}
+		boolean hasNum = false;
+		boolean hasSpecialChars = false;
+		boolean hasChars = false;
+		for (char c : password.toCharArray()) {
+
+			if (c >= 48 && c <= 57) {
+
+				hasNum = true;
+			}
+			if ((c >= 33 && c <= 47) ||
+					(c >= 58 && c <= 64) ||
+					(c >= 91 && c <= 96) ||
+					(c >= 123 && c <= 126)) {
+
+				hasSpecialChars = true;
+			}
+			if ((c >= 65 && c <= 90) || (c >= 97 && c <= 122)) {
+
+				hasChars = true;
+			}
+		}
+		if (!hasNum) {
+
+			return false;
+		}
+		if (!hasSpecialChars) {
+
+			return false;
+		}
+		if (!hasChars) {
+
+			return false;
+		}
+		return true;
 	}
 }
--- src/test/java/tdd/logic/password/PasswordValidatorTest.java
+++ src/test/java/tdd/logic/password/PasswordValidatorTest.java
@@ -13,7 +13,9 @@ class PasswordValidatorTest {
 	@Test
 	void passwordIsValid() {
 
-		assertTrue(PasswordValidator.validatePassword("haha"));
+		assertTrue(PasswordValidator.validatePassword("X8zS0kayAs3tze1rI*jSrlRV0a7UQtL"));
+		assertTrue(PasswordValidator.validatePassword(">^U-~b2`!G=UyM5VGl('&x<1\"mBA\",w"));
+		assertTrue(PasswordValidator.validatePassword("tçÀ¾÷ÍµsØ7¦OXª8\\=TÃG}:P«jiòUæ!"));
 	}
 
 	@Test
@@ -29,35 +31,44 @@ class PasswordValidatorTest {
 	void passwordIsTooShort() {
 
 		assertFalse(PasswordValidator.validatePassword("123456"));
-		assertTrue(PasswordValidator.validatePassword("12345678"));
+		assertFalse(PasswordValidator.validatePassword("12345678910"));
+		assertTrue(PasswordValidator.validatePassword("123456789102aoeuaoeu!@#$!@#$"));
 		StringBuilder sb = new StringBuilder();
-		for (int i = 0; i < Integer.MAX_VALUE; i++) {
+		for (int i = 0; i < Integer.MAX_VALUE / 10; i++) {
 			sb.append("a");
 		}
+		sb.append(1234);
+		sb.append("1@#$!@#$");
 		assertTrue(PasswordValidator.validatePassword(sb.toString()));
 	}
 
 	@Test
 	void passwordDoesNotContainDigits() {
 
-		fail();
+		assertFalse(PasswordValidator.validatePassword("E@_HHeAVUE%_[C-bXdpl`?YUoIo;Po"));
+		assertFalse(PasswordValidator.validatePassword("±ûÝ?yÑßXÔ`ö¤äñyå'ùñÔî§KA£b{H¤"));
+		assertTrue(PasswordValidator.validatePassword("tçÀ¾÷ÍµsØ7¦OXª8\\=TÃG}:P«jiòUæ!e"));
 	}
 
 	@Test
 	void passwordDoesNotContainSpecialCharacters() {
 
-		fail();
+		assertTrue(PasswordValidator.validatePassword("E@1_HHeAVUE%_[C-bXdpl`?YU3oIo;Po"));
+		assertFalse(PasswordValidator.validatePassword("oocFyKZHWurrLwPDwChLnClAKPlFSa"));
 	}
 
 	@Test
 	void passwordDoesNotContainChars() {
 
-		fail();
+		assertFalse(PasswordValidator.validatePassword("1234123412341234**"));
+		assertFalse(PasswordValidator.validatePassword(",:*)@}'/)@_)-<:,:[123=^??(_\\!*~&*"));
 	}
 
 	@Test
 	void passwordDoesNotHaveAMixOfCharsAndSymbols() {
 
-		fail();
+		assertFalse(PasswordValidator.validatePassword("12341234123412{}]['234][][21]1234][**"));
+		assertFalse(PasswordValidator.validatePassword(",:*)@}'/)@_)-<:,:[123=^??(_\\!*~&1234!@#$1234*"));
+		assertTrue(PasswordValidator.validatePassword("123412!@#$!@#$aouikao41234EOUI12341AOUI',341234"));
 	}
 }
