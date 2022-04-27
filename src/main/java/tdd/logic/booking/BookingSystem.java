e126e677f0016e48caab0ddf7a7f220b8d0fb233
Martin Nestorov <mnestorov@protonmail.com>
2022-04-27 11:13:20 +0300

    Add booking system logic

--- src/main/java/tdd/logic/booking/BookingSystem.java
+++ src/main/java/tdd/logic/booking/BookingSystem.java
@@ -1,3 +1,43 @@
 package tdd.logic.booking;
 
-public class BookingSystem {}
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+import org.apache.commons.lang3.Validate;
+
+public class BookingSystem {
+
+	private final Set<Integer> bookedHours;
+
+	public BookingSystem(Integer... hoursToBook) {
+
+		bookedHours = new HashSet<>();
+		addBookings(hoursToBook);
+	}
+
+	public List<Integer> getBookedHours() {
+
+		return bookedHours.stream().collect(Collectors.toList());
+	}
+
+	public void addBooking(Integer toBook) {
+
+		Validate.notNull(toBook, "Booking hour cannot be null!");
+		Validate.isTrue(toBook >= 0 && toBook <= 24, "Booking cannot be for an illegal hour");
+
+		if (!bookedHours.add(toBook)) {
+
+			throw new IllegalArgumentException(String.format("Cannot add a booking to the same hour [%d]!", toBook));
+		}
+	}
+
+	public void addBookings(Integer... hoursToBook) {
+
+		for (Integer hour : hoursToBook) {
+
+			addBooking(hour);
+		}
+	}
+}
