7b88b243e15930b0ecc802e109db3fc30241e890
Martin Nestorov <mnestorov@protonmail.com>
2022-04-27 12:25:50 +0300

    Test booking system with map api

--- src/test/java/tdd/logic/booking/BookingSystemTest.java
+++ src/test/java/tdd/logic/booking/BookingSystemTest.java
@@ -4,11 +4,14 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertFalse;
-import static org.junit.jupiter.api.Assertions.assertNotNull;
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.empty;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.hasSize;
+import static org.hamcrest.Matchers.is;
+import static org.hamcrest.Matchers.not;
+import static org.hamcrest.Matchers.notNullValue;
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.junit.jupiter.api.Assertions.assertTrue;
 
 class BookingSystemTest {
 
@@ -17,17 +20,26 @@ class BookingSystemTest {
 	@Test
 	void getBookedHours() {
 
-		bookingSystem = new BookingSystem();
-		List<Integer> bookedHours = bookingSystem.getBookedHours();
+		bookingSystem = new BookingSystem("Hotel");
+		List<Integer> bookedHours = bookingSystem.getBookedHours("Hotel");
 
-		assertTrue(bookedHours.isEmpty());
+		assertThat(bookedHours, empty());
 
-		bookingSystem = new BookingSystem(13, 14, 15, 20, 9, 8);
-		List<Integer> bookedHours2 = bookingSystem.getBookedHours();
+		bookingSystem = new BookingSystem("Circus", 13, 14, 15, 20, 9, 8);
+		List<Integer> bookedHours2 = bookingSystem.getBookedHours("Circus");
 
-		assertNotNull(bookedHours2);
-		assertFalse(bookedHours2.isEmpty());
-		assertEquals(6, bookedHours2.size());
+		assertThat(bookedHours2, notNullValue());
+		assertThat(bookedHours2, is(not(empty())));
+		assertThat(bookedHours2, hasSize(6));
+
+		bookingSystem = new BookingSystem("Dancing", 8, 9, 10);
+		bookingSystem.addBookings("Jumping", 8, 9, 10);
+
+		List<Integer> dancing = bookingSystem.getBookedHours("Dancing");
+		List<Integer> jumping = bookingSystem.getBookedHours("Jumping");
+		assertThat(dancing, equalTo(jumping));
+		assertThat(dancing, hasSize(3));
+		assertThat(jumping, hasSize(3));
 	}
 
 	@Test
@@ -35,31 +47,32 @@ class BookingSystemTest {
 
 		assertThrows(
 				IllegalArgumentException.class,
-				() -> new BookingSystem(13, 14, 15, 13));
+				() -> new BookingSystem("Opera", 13, 14, 15, 13));
 
 		bookingSystem = new BookingSystem();
 
-		bookingSystem.addBooking(13);
-		bookingSystem.addBooking(14);
-		bookingSystem.addBooking(15);
-		assertThrows(IllegalArgumentException.class, () -> bookingSystem.addBooking(14));
+		bookingSystem.addBooking("Ice skating", 13);
+		bookingSystem.addBooking("Ice skating", 14);
+		bookingSystem.addBooking("Ice skating", 15);
+		assertThrows(IllegalArgumentException.class, () -> bookingSystem.addBooking("Ice skating", 14));
 
 		bookingSystem = new BookingSystem();
 
-		assertThrows(IllegalArgumentException.class, () -> bookingSystem.addBookings(13, 14, 15, 15));
+		assertThrows(IllegalArgumentException.class, () -> bookingSystem.addBookings("Car wash", 13, 14, 15, 15));
 	}
 
 	@Test
 	void passWrongDataToBooking() {
 
-		assertThrows(NullPointerException.class, () -> new BookingSystem(1, 2, 3, null));
-		assertThrows(NullPointerException.class, () -> new BookingSystem().addBooking(null));
-		assertThrows(NullPointerException.class, () -> new BookingSystem().addBookings(1, 2, null));
+		assertThrows(NullPointerException.class, () -> new BookingSystem("Library", 1, 2, 3, null));
+		assertThrows(NullPointerException.class, () -> new BookingSystem().addBooking("Cooking", null));
+		assertThrows(NullPointerException.class, () -> new BookingSystem().addBookings("Cleaning", 1, 2, null));
 
-		assertThrows(IllegalArgumentException.class, () -> new BookingSystem(-1, -20, 200, 1012, Integer.MAX_VALUE));
-		assertThrows(IllegalArgumentException.class, () -> new BookingSystem().addBooking(Integer.MIN_VALUE));
-		assertThrows(IllegalArgumentException.class, () -> new BookingSystem().addBookings(30, 32, -30));
+		assertThrows(IllegalArgumentException.class, () ->
+				new BookingSystem("Reading", -1, -20, 1012, 200, Integer.MAX_VALUE));
+		assertThrows(IllegalArgumentException.class, () -> new BookingSystem().addBooking("Coding", Integer.MIN_VALUE));
+		assertThrows(IllegalArgumentException.class, () -> new BookingSystem().addBookings("Movies", 30, 32, -30));
 
-		new BookingSystem(0, 24);
+		new BookingSystem("Drifting", 0, 24);
 	}
 }
